9a4af56270f67854de23ca89381b50dc72df0355
Jovi Simons <jovisimons009@gmail.com>
2023-06-01 11:31:26 +0200

    update user

--- src/main/java/jovisimons/dekeet/EventService/Listener.java
+++ src/main/java/jovisimons/dekeet/EventService/Listener.java
@@ -1,6 +1,7 @@
 package jovisimons.dekeet.EventService;
 
 import jovisimons.dekeet.EventService.service.EventService;
+import jovisimons.dekeet.common.model.UserMsgName;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,5 +16,10 @@ public class Listener {
     @RabbitListener(queues = {"q.event_userDelete"})
     public void onUserDelete(String uid) {
         svc.deleteUser(uid);
+    }
+
+    @RabbitListener(queues = {"q.event_userUpdate"})
+    public void onUserUpdate(UserMsgName user) {
+        svc.updateUser(user);
     }
 }
